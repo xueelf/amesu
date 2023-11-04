d4cb9d3fc0c3760bbac860d0b36fc3429f09edcb
xueelf <xueelf@outlook.com>
2023-11-04 16:32:39 +0800

    feat: add logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,18 @@
+import log4js, { Level, Logger } from 'log4js';
+
+/** 日志等级 */
+export type LogLevel = 'ALL' | 'TRACE' | 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'FATAL';
+
+const loggerMap: Map<string, Logger> = new Map();
+
+export function createLogger(name: string, level: Level | LogLevel): Logger {
+  const logger = log4js.getLogger(`[${name}]`);
+  logger.level = level;
+
+  loggerMap.set(name, logger);
+  return logger;
+}
+
+export function getLogger(name: string): Logger | undefined {
+  return loggerMap.get(name);
+}
