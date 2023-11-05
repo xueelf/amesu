7a7c4cb0c91e2cd46ab43d053894b15a50dbe04e
xueelf <xueelf@outlook.com>
2023-11-06 07:06:26 +0800

    feat: add interceptor

--- src/client/request.ts
+++ src/client/request.ts
@@ -1,4 +1,5 @@
 import type { Logger } from 'log4js';
+
 import { EventEmitter } from 'node:events';
 import { Token } from '@/client/token.js';
 import { getLogger } from '@/utils/logger.js';
@@ -7,6 +8,8 @@ import { deepAssign, objectToString } from '@/utils/common.js';
 export type Data = Record<string, unknown>;
 export type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 export type Instance = ReturnType<InstanceType<typeof Request>['create']>;
+export type RequestInterceptor = (config: RequestConfig) => RequestConfig | Promise<RequestConfig>;
+export type ResponseInterceptor = (result: Partial<Result>) => Partial<Result> | Promise<Partial<Result>> | undefined;
 
 export interface RequestConfig extends RequestInit {
   baseURL?: string;
@@ -31,10 +34,23 @@ export class RequestError extends Error {
 
 export class Request extends EventEmitter {
   private logger: Logger;
+  private requestInterceptors: RequestInterceptor[];
+  private responseInterceptors: ResponseInterceptor[];
 
   constructor(public token: Token) {
     super();
+
     this.logger = getLogger(token.config.appid);
+    this.requestInterceptors = [];
+    this.responseInterceptors = [];
+  }
+
+  public useRequestInterceptor(interceptor: RequestInterceptor) {
+    this.requestInterceptors.push(interceptor);
+  }
+
+  private useResponseInterceptor(interceptor: ResponseInterceptor) {
+    this.responseInterceptors.push(interceptor);
   }
 
   public async base<T>(config: RequestConfig): Promise<Result<T>> {
@@ -45,6 +61,10 @@ export class Request extends EventEmitter {
     };
     config = <RequestConfig>deepAssign(defaultConfig, config);
 
+    // 请求拦截器
+    for (const interceptor of this.requestInterceptors) {
+      config = await interceptor(config);
+    }
     this.logger.trace('开始发起网络请求...');
 
     const response = await fetch((config.baseURL ?? '') + config.url, config);
@@ -60,6 +80,10 @@ export class Request extends EventEmitter {
     try {
       result.data = <Data>await response.json();
 
+      // 响应拦截器
+      for (const interceptor of this.responseInterceptors) {
+        // TODO: ／人◕ ‿‿ ◕人＼ 目前用不到
+      }
       this.emit('response', result);
       this.logger.debug(`Response: ${objectToString(result.data)}`);
     } catch (error) {
